62684516a4464a49794cf3fc4894bde4f01c1e7b
caoZongRui <1366801568@qq.com>
2020-06-23 15:49:45 +0800

    测试删除target前所有的代码提交

--- cloud-provider-payment8001/src/main/java/com/czr/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/czr/springcloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author czr
@@ -33,44 +34,52 @@ public class PaymentController {
     private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/create")
-    public CommonResult create(@RequestBody Payment payment){
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("*******插入结果：****");
-        if (result > 0){
-            return new CommonResult(200,"插入数据库成功，端口号serverPort："+serverPort,result);
-        }else {
-            return new CommonResult(444,"插入数据库失败",null);
+        if (result > 0) {
+            return new CommonResult(200, "插入数据库成功，端口号serverPort：" + serverPort, result);
+        } else {
+            return new CommonResult(444, "插入数据库失败", null);
         }
     }
 
     @GetMapping(value = "/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id){
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*******查询结果：****");
-        if (payment != null){
-            return new CommonResult(200,"查询成功，端口号serverPort："+serverPort,payment);
-        }else {
-            return new CommonResult(444,"没有对应记录ID：" + id,null);
+        if (payment != null) {
+            return new CommonResult(200, "查询成功，端口号serverPort：" + serverPort, payment);
+        } else {
+            return new CommonResult(444, "没有对应记录ID：" + id, null);
         }
     }
 
     @GetMapping(value = "/discovery")
-    public Object discovery(){
+    public Object discovery() {
         List<String> services = discoveryClient.getServices();
-        for (String element : services){
+        for (String element : services) {
             log.info("******element:" + element);
         }
-
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-        for (ServiceInstance instance : instances){
+        for (ServiceInstance instance : instances) {
             log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
         }
         return this.discoveryClient;
     }
 
     @GetMapping(value = "/lb")
-    public String getPaymentLB()
-    {
+    public String getPaymentLB() {
+        return serverPort;
+    }
+
+    @GetMapping(value = "/feign/timeout")
+    public String paymentFeignTimeout() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return serverPort;
     }
 
