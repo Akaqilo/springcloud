403b6206ae959e3a8593b21254d5e1464daa20fd
caoZongRui <1366801568@qq.com>
2020-07-30 09:19:25 +0800

    添加工具类

---
+++ cloud-api-commons/src/main/java/com/czr/springcloud/util/Test1.java
@@ -0,0 +1,20 @@
+package com.czr.springcloud.util;
+
+import cn.hutool.crypto.SmUtil;
+
+/**
+ * @author czr
+ * @Description: 类
+ * @create 2020-07-14 18:28
+ * @servion 1.0
+ */
+public class Test1 {
+
+
+    // public static void main(String[] args) {
+    //     String digestHex1 = SmUtil.sm3("itfaster");
+    //     String digestHex2 = SmUtil.sm3("itfaster");
+    //     System.out.println(digestHex1.substring(10, 26));
+    //     System.out.println(digestHex2);
+    // }
+}
